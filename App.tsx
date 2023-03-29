0fdb9989f524904b475d8e3d9a870dd2d869e0c4
Alaa Naeeim <113712408+alaanaeeim@users.noreply.github.com>
2023-03-29 17:46:17 +0200

    change into app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text} from 'react-native';
 import React from 'react';
-import QuestionsBoard from './src/screens/QuestionsBoard/QuestionsBoard';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <QuestionsBoard />
+      <Text>Learning App</Text>
     </View>
   );
 }
